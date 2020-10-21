937e684675526fb47cc3cf050769b07b3f610de3
J. Erik <jerik@gmx.net>
2020-10-22 00:08:30 +0200

    fixed meta data inconsistency s/money/py-money/
    Fixed: WARNING: Generating metadata for package py-money produced metadata for project name money.WARNING: Generating metadata for package py-money produced metadata for project name money. Fix your #egg=py-money fragments.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="money",
+    name="py-money",
     version="0.1.1",
     author="Mirko Dziadzka",
     author_email="mirko.dziadzka@gmail.com",
